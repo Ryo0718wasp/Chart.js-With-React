4de794e58f3355dc3831254db3b08113d23303c9
ldl093 <sugimoto@liddell.tokyo>
2020-07-28 16:04:38 +0900

    add some propaties

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from './components/Chart';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,17 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={() => setCount(count + 1)}>Plus</button>
-        <button onClick={() => setCount(count - 1)}>minus</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Chart legendPosition="bottom" />
     </div>
   );
 }
